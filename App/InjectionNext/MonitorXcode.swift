0025f0b2a51f2a2112258a696bc3d9fb3ea67100
John Holdsworth <1786033+johnno1962@users.noreply.github.com>
2025-02-23 09:37:39 +0100

    Deduping.

--- App/InjectionNext/MonitorXcode.swift
+++ App/InjectionNext/MonitorXcode.swift
@@ -28,12 +28,11 @@ class MonitorXcode {
     // The service to recompile and inject a source file.
     var recompiler = NextCompiler()
 
-    func debug(_ msg: String) {
+    func debug(_ what: Any..., separator: String = " ") {
         #if DEBUG
-        //print(msg)
+//        print(what, separator: separator)
         #endif
     }
-    
     init(args: String = "") {
         var args = args
         #if DEBUG
@@ -85,7 +84,8 @@ class MonitorXcode {
                         if out.hasPrefix("/") &&
                             !FileManager.default.fileExists(atPath: out),
                            let data = out.data(using: .macOSRoman),
-                           let recovered = String(data: data, encoding: .utf8) {
+                           let recovered = String(data: data, encoding: .utf8),
+                           FileManager.default.fileExists(atPath: recovered) {
                             out = recovered
                         }
                         if strstr(start+1, escaped) != nil {
@@ -115,8 +115,9 @@ class MonitorXcode {
                  line == "  key.request: source.request.relatedidents,") &&
                 xcodeStdout.readLine() == "  key.compilerargs: [" ||
                 line == "  key.compilerargs: [" {
-                var swiftFiles = "", args = [String](),
-                    fileCount = 0, workingDir = "/tmp"
+                var swiftFiles = "", args = [String](), fileCount = 0,
+                    workingDir = "/tmp", configDirs = Set<String>()
+
                 while var arg = readQuotedString() {
                     let llvmIncs = "/llvm-macosx-arm64/lib"
                     if arg.hasPrefix("-I"), arg.contains(llvmIncs) {
@@ -125,16 +126,19 @@ class MonitorXcode {
                     }
                     if args.last == "-F" && arg.hasSuffix("/PackageFrameworks") {
                         Unhider.packageFrameworks = arg
+
                         let frameworksURL = URL(fileURLWithPath: arg)
-                        let productsDir = frameworksURL.deletingLastPathComponent()
+                        let derivedData = frameworksURL.deletingLastPathComponent()
                             .deletingLastPathComponent().deletingLastPathComponent()
                             .deletingLastPathComponent().deletingLastPathComponent()
-                            .appendingPathComponent("Build/Products")
+                        let productsDir =
+                            derivedData.appendingPathComponent("Build/Products")
                         let platform = InjectionServer
                             .currentClient?.platform ?? "iPhoneSimulator"
                         if let configs = Glob(pattern: productsDir.path +
                                                "/*-" + platform.lowercased()) {
-                            for other in configs {
+                            for other in configs
+                                where configDirs.insert(other).inserted {
                                 args += [other, "-F"]
                             }
                         }
@@ -173,12 +177,14 @@ class MonitorXcode {
                         args.append(arg)
                     }
                 }
+
                 guard !args.isEmpty, let source =
                         readQuotedString() ?? readQuotedString(),
                       !source.contains("\\n") else {
                     continue
                 }
                 lastSource = source
+
                 if let previous = recompiler
                     .compilations[source]?.arguments ?? lastArguments,
                     args == previous {
@@ -193,9 +199,11 @@ class MonitorXcode {
                 } else {
                     lastFilelist = swiftFiles
                 }
+
                 print("Updating \(args.count) args with \(fileCount) swift files "+source+" "+line)
-                let update = NextCompiler.Compilation(
-                    arguments: args, swiftFiles: swiftFiles, workingDir: workingDir)
+                let update = NextCompiler.Compilation(arguments: args,
+                    swiftFiles: swiftFiles, workingDir: workingDir)
+ 
                 // The folling line should be on the compileQueue
                 // but it seems to provoke a Swift compiler bug.
                 self.recompiler.compilations[source] = update
